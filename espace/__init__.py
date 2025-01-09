b305d7cae3860ea846ff5c1abaae6b7beb4324e0
csakr <csakr@nvidia.com>
2025-01-09 15:15:08 -0800

    add license to headers

--- espace/__init__.py
+++ espace/__init__.py
@@ -1,5 +1,12 @@
 '''
-Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-FileCopyrightText: Copyright (c) <2025> NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+
+ NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+ property and proprietary rights in and to this material, related
+ documentation and any modifications thereto. Any use, reproduction,
+ disclosure or distribution of this material and related documentation
+ is subject to the NVIDIA license agreement.
 
 ESPACE Toolkit: init file.
 '''
